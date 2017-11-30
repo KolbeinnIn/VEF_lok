39fb12e08cdfc3923296972f20c5dc78afdcf55e
KolbeinnIn <kolbeinnin264@nemi.tskoli.is>
2017-11-30 11:08:04 +0000

    database

--- index.py
+++ index.py
@@ -3,8 +3,28 @@
 # Lokaverkefni - VEF
 
 from bottle import *
+from pymysql import *
 from beaker.middleware import SessionMiddleware
 
+
+db = Connect(host="tsuts.tskoli.is", user="0908002640", password="mypassword", db="0908002640_vefLok")
+cursor = db.cursor()
+cursor.execute("select * from vorur")
+numrows = int(cursor.rowcount)
+vorur = {}
+listivorur = []
+kulVara = 0
+teljari = 0
+for x in range(numrows):
+    row = cursor.fetchone()
+    print(row)
+    if row:
+        teljari += 1
+        vorur = {}
+        vorur["voruid"] = row[0]
+        vorur["name"] = row[1]
+        vorur["verd"] = row[2]
+        listivorur.append(vorur)
 
 @route('/static/<filepath>')
 def static(filepath):
@@ -21,11 +41,18 @@ sess = {
     'session.auto': True
 }
 app = SessionMiddleware(app(), sess)
-vorur = [{'voruid': 1, 'name': 'Vara 1', 'price': 1000},
+vorurasd = [{'voruid': 1, 'name': 'Vara 1', 'price': 1000},
          {'voruid': 2, 'name': 'Vara 2', 'price': 2000},
          {'voruid': 3, 'name': 'Vara 3', 'price': 3000},
          {'voruid': 4, 'name': 'Vara 4', 'price': 4000}]
+print(vorurasd)
+print(listivorur)
+@route('/')
+def index():
+    return template('test.tpl', row=row, numrows=numrows, vorur=vorur, cursor=cursor)
 
+
+"""
 @route('/')
 def index():
     s = request.environ.get('beaker.session')
@@ -88,5 +115,5 @@ def eyda_ur_korfu():
     session.delete()
     return redirect('/karfa')
 
-
+"""
 run(host='localhost', port=8080, debug=True, app=app)
---
+++ views/test.tpl
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <intercept-url pattern="/favicon.ico" access="ROLE_ANONYMOUS" />
+    <title>Verkefni 8</title>
+    <meta charset="utf-8">
+    <link href="/static/css/styles.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+   <main>
+       <header>
+            <h1>Vefverslun</h1>
+       </header>
+        % for i in range(numrows):
+            %row = cursor.fetchone()
+            <p>{{row}}</p>
+        % end
+  </main>
+</body>
+</html>
